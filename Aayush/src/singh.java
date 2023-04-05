0c57818097bca3b79373796bde9529fd2800e31a
Sdac <Sdac@Sdac-PC>
2023-04-05 16:01:39 +0530

    version 3

--- Aayush/src/singh.java
+++ Aayush/src/singh.java
@@ -1,6 +1,7 @@
 
 public class singh {
-public static void main(String[] args) {
-	System.out.println("verion name");
-}
+	public static void main(String[] args) {
+		System.out.println("hi there");
+	}
+
 }
